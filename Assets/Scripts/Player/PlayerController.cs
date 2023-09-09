c462841f1207ee7ee7b5c465067757cfd954f95b
JuanMBrdch <juanmbordachar@gmail.com>
2023-09-09 15:32:36 -0300

    scripts

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerModel _model;
+    
+    
+    private void Start()
     {
-        
+        if (IsOwner)
+        {
+            _model = GetComponent<PlayerModel>();
+        }
+        else
+        {
+            this.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        
+        if (!IsOwner) return;
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _model.Move(dir);
     }
 }
--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Components;
 
-public class PlayerModel : MonoBehaviour
+public class PlayerModel : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    private Rigidbody _rb;
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector3 dir)
     {
-        
+        dir *= speed;
+        dir.y = _rb.velocity.y;
+        _rb.velocity = dir;
     }
 }
